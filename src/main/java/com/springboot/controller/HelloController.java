713ed457814aef6c5a3741623c2e382f68af2d39
yugal kishor <yugal.kishor@serosoft.in>
2025-03-05 18:36:33 +0530

    first commit

---
+++ src/main/java/com/springboot/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.springboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String getMessage() {
+        return "Hello yugal rajput !! ";
+    }
+
+    @GetMapping("/welcome")
+    public String getMessage1() {
+        return "Welcome yugal rajput !! ";
+    }
+
+
+}
